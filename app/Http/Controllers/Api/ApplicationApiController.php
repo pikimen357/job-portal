5ae00f1a292290f6dba3279eb1815356ece4dc25
pikimen357 <vidkyadhipradana2004@mail.ugm.ac.id>
2025-11-29 09:40:45 +0700

    Job Portal API update

--- app/Http/Controllers/Api/ApplicationApiController.php
+++ app/Http/Controllers/Api/ApplicationApiController.php
@@ -47,28 +47,4 @@ class ApplicationApiController extends Controller
                 "application" => $app]
             , 201);
     }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
